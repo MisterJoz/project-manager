bf5cd33f2937bba1fec268af05312a2a782a2f3f
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-05-20 16:58:14 -0500

    udpate staticfile dir

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -130,5 +130,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'project_manager/static')
+    os.path.join(BASE_DIR, 'static')
 ]
